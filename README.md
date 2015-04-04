ca07392692205a7df2dcf99348026501a39553fc
Mark Saroufim <marksaroufim@gmail.com>
2015-04-04 14:18:31 -0700

    added working mergesort with testcase

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Python Undergrad DataStructures and Algo Implementations
 
 Since people seem to care about this stuff, I've added implementations of
-important undergrad algos. Open an isse if you'd like me to implement a specific
+important undergrad algos in Python. Open an isse if you'd like me to implement a specific
 one.
 
 So far I have:
--- mergesort.py
+++ mergesort.py
@@ -1,26 +1,54 @@
-#mergesort function takes in [a] and returns sorted [a]
-def mergesort(alist):
-	print("sorting",alist)
-	if(len(alist) > 1):
-		mid = len(alist) / 2
-		lefthalf = alist[:mid] 
-		righhalf = alist[mid:]
-		mergesort(lefthalf)
-		mergesort(righhalf)
+class MergeSort(object):
+    def merge(self,left, right):
+        nl = len(left)
+        nr = len(right)
+        result = [0]*(nl+nr)
+        i=0
+        j=0
+        for k in range(len(result)):
+            if nl>i and nr>j:
+                if left[i] <= right[j]:
+                    result[k]=left[i]
+                    i+=1
+                else:
+                    result[k]=right[j]
+                    j+=1
+            elif nl==i:
+                result[k] = right[j]
+                j+=1
+            else: #nr>j:
+                result[k] = left[i]
+                i+=1
+        return result
 
-		i = 0
-		j = 0
-		k = 0
-
-		while i < len(lefthalf) and j < len(righhalf):
-
-
-
-
+    def sort(self,arr):
+        n = len(arr)
+        if n<=1:
+            return arr 
+        left = self.sort(arr[:n/2])
+        right = self.sort(arr[n/2:] )
+        return self.merge(left, right)
 
 
-#Setting up test case, should look into automated 
-test = [2,500,1,-2,8,9,1,2,4,102]
-a = mergesort(test)
-assert(a == test.sort())
-print("It's all good")
+#Test it out
+def main():
+    import random
+    import time
+    a = range(100000)
+    random.shuffle(a)
+    MergeSortInstance = MergeSort()
+
+    t0 = time.time()
+    result = MergeSortInstance.sort(a)
+    t1 = time.time()
+    total_time = t1 - t0
+    #assert against the builtin sort method
+    #result of builtin sorting algorithm
+
+    #result of 
+    print result
+    print("Amount of time taken is",total_time)
+
+
+if __name__ == '__main__':
+    main()
