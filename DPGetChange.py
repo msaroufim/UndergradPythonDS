3edbdf81a4164f60bea42654cbe9580a25d54c7b
Mark Saroufim <marksaroufim@gmail.com>
2015-04-06 21:59:49 -0700

    added memoized and dp programming solution for change problem

--- DPGetChange.py
+++ DPGetChange.py
@@ -10,13 +10,33 @@ class MakeChange:
 	            minCoins = numCoins
 	   return minCoins
 
-	def recMCMemoized(coinValueList,change):
-		#to implement
-		pass
 
-	def DPMC(coinValueList,change):
-		#to implement
-		pass
+
+def recDCMemoized(coinValueList,change,knownResults):
+   minCoins = change
+   if change in coinValueList:
+      knownResults[change] = 1
+      return 1
+   elif knownResults[change] > 0:
+      return knownResults[change]
+   else:
+       for i in [c for c in coinValueList if c <= change]:
+         numCoins = 1 + recDCMemoized(coinValueList, change-i,
+                              knownResults)
+         if numCoins < minCoins:
+            minCoins = numCoins
+            knownResults[change] = minCoins
+   return minCoins
+
+
+def DPMC(coinValueList,change,minCoins):
+   for cents in range(change+1):
+      coinCount = cents
+      for j in [c for c in coinValueList if c <= cents]:
+            if minCoins[cents-j] + 1 < coinCount:
+               coinCount = minCoins[cents-j]+1
+      minCoins[cents] = coinCount
+   return minCoins[change]
 
 
 if __name__ == "__main__":
